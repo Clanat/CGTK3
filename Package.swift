33bef3a5581d6dc44fe3f759e4136e1337b86388
Nick Zatsepilov <clanat001@gmail.com>
2018-03-18 16:04:22 +0600

    Update Package.swift

--- Package.swift
+++ Package.swift
@@ -1,5 +1,8 @@
+// swift-tools-version:4.0
 import PackageDescription
 
 let package = Package(
-  name: "CGTK3"
+  name: "CGTK3",
+  pkgConfig: "gtk+-3.0",
+  providers: [.apt("libgtk-3-dev clang")]
 )
